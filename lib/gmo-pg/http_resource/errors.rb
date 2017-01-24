b7a71f68707176ac2b4ddb60c65877802f588823
kissy2go <kissy2go@gmail.com>
2017-01-24 17:55:16 +0900

    Add GMO::PG::Errors::Record

--- lib/gmo-pg/http_resource/errors.rb
+++ lib/gmo-pg/http_resource/errors.rb
@@ -1,6 +1,12 @@
 module GMO
   module PG
     class Errors
+      class Record < Struct.new(:err_code, :err_info)
+        def to_error
+          GMO::PG::Error.from_api_error(err_code, err_info)
+        end
+      end
+
       include Enumerable
 
       SEPARATOR = '|'
@@ -14,7 +20,7 @@ module GMO
         return to_enum unless block_given?
         return nil if @err_code.nil? && @err_info.nil?
         @err_code.zip(@err_info).each do |(err_code, err_info)|
-          yield err_code, err_info
+          yield Record.new(err_code, err_info)
         end
       end
     end
--- spec/gmo-pg/http_resource/errors_spec.rb
+++ spec/gmo-pg/http_resource/errors_spec.rb
@@ -15,9 +15,26 @@ RSpec.describe GMO::PG::Errors do
 
     context 'with block' do
       it 'yields per error code and info pair' do
-        expected_args = err_code.split('|').zip(err_info.split('|'))
+        expected_args = err_code.split('|').zip(err_info.split('|')).map do |(err_code, err_info)|
+          GMO::PG::Errors::Record.new(err_code, err_info)
+        end
         expect { |b| errors.each(&b) }.to yield_successive_args *expected_args
       end
+    end
+  end
+end
+
+RSpec.describe GMO::PG::Errors::Record do
+  describe '#to_error' do
+    subject { record.to_error }
+
+    let(:record) { GMO::PG::Errors::Record.new('E00', 'E00000000') }
+
+    it 'delegates GMO::PG::Error::from_api_error with err_code and err_info' do
+      expect(GMO::PG::Error).to receive(:from_api_error)
+        .with(record.err_code, record.err_info)
+        .and_return(:ok)
+      expect(subject).to eq :ok
     end
   end
 end
