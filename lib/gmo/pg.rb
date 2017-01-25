b74a13ca45a007513cc6a8c3c5fa190c0fd73dfa
kissy2go <kissy2go@gmail.com>
2017-01-25 18:47:22 +0900

    Change directory tree

---
+++ lib/gmo/pg.rb
@@ -0,0 +1,28 @@
+require 'gmo/pg/version'
+
+require 'gmo/pg/http_resource'
+require 'gmo/pg/api_endpoint'
+require 'gmo/pg/dispatcher'
+require 'gmo/pg/error'
+
+module GMO
+  module PG
+    @open_timeout       = 60
+    @read_timeout       = 90
+    @raise_on_api_error = true
+
+    class << self
+      attr_accessor :base_url, :open_timeout, :read_timeout, :proxy, :raise_on_api_error
+    end
+
+    def self.connect(&block)
+      dispatcher = Dispatcher.new(base_url) do |d|
+        d.open_timeout       = open_timeout
+        d.read_timeout       = read_timeout
+        d.raise_on_api_error = raise_on_api_error
+        d.use_proxy proxy if proxy
+      end
+      dispatcher.connect(&block)
+    end
+  end
+end
---
+++ lib/gmo/pg/http_resource.rb
@@ -0,0 +1,4 @@
+require 'gmo/pg/http_resource/payload'
+require 'gmo/pg/http_resource/generic_request'
+require 'gmo/pg/http_resource/generic_response'
+require 'gmo/pg/http_resource/errors'
