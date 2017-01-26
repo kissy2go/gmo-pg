5c641d1366f2d3d8a73e725d9f322bbaa021bc87
kissy2go <kissy2go@gmail.com>
2017-01-26 12:58:06 +0900

    Update acceptance test

--- spec/acceptance/steps/feature_steps.rb
+++ spec/acceptance/steps/feature_steps.rb
@@ -22,7 +22,7 @@ module FeatureSteps
   def vcr_options(overrides = {})
     {
       re_record_interval: 60 * 60 * 24 * 7, # 7 days
-      match_requests_on: %i{ method path body },
+      match_requests_on: %i{ method path },
       allow_unused_http_interactions: true,
       record: :new_episodes,
     }.merge(overrides)
@@ -45,10 +45,10 @@ module FeatureSteps
   end
 
   def order_id
-    @order_id ||= 'OID-' + [*('a'..'z'), *('A'..'Z'), *('0'..'9')].sample(23).join('')
+    @order_id ||= GMO::PG::Util::OrderID.generate(prefix: 'OID-')
   end
 
   def member_id
-    @member_id ||= 'MEM-' + [*('a'..'z'), *('A'..'Z'), *('0'..'9'), '-', '@', '_', '.'].sample(56).join('')
+    @member_id ||= GMO::PG::Util::MemberID.generate(prefix: 'MEM-')
   end
 end
