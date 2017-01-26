202217b76b8594ec8f521bb46e81b7f86fa01865
kissy2go <kissy2go@gmail.com>
2017-01-26 11:05:58 +0900

    Support to connect specific remote server

--- lib/gmo/pg.rb
+++ lib/gmo/pg.rb
@@ -15,7 +15,7 @@ module GMO
       attr_accessor :base_url, :open_timeout, :read_timeout, :proxy, :raise_on_api_error
     end
 
-    def self.connect(&block)
+    def self.connect(base_url = self.base_url, &block)
       dispatcher = Dispatcher.new(base_url) do |d|
         d.open_timeout       = open_timeout
         d.read_timeout       = read_timeout
--- spec/gmo/pg_spec.rb
+++ spec/gmo/pg_spec.rb
@@ -10,6 +10,14 @@ RSpec.describe GMO::PG do
         expect(dispatcher.read_timeout).to eq GMO::PG.read_timeout
         expect(dispatcher.raise_on_api_error).to eq GMO::PG.raise_on_api_error
       end
+
+      GMO::PG.connect 'https://another.example.com' do |dispatcher|
+        expect(dispatcher).to be_connected
+        expect(dispatcher.base_url).to eq 'https://another.example.com'
+        expect(dispatcher.open_timeout).to eq GMO::PG.open_timeout
+        expect(dispatcher.read_timeout).to eq GMO::PG.read_timeout
+        expect(dispatcher.raise_on_api_error).to eq GMO::PG.raise_on_api_error
+      end
     end
   end
 end
